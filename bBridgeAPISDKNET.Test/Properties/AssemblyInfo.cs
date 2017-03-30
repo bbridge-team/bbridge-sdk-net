be0a3f618317f973e75f25aac3654413be6bd44f
farseev <farseev@gmail.com>
2017-03-29 22:59:24 -0700

    Now can be build for both .Net Framework Classic (Windows only) and .Net Core (Cross-Platform)

---
+++ bBridgeAPISDKNET.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("bBridgeAPISDKNET.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bBridgeAPISDKNET.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5289fa2e-1bba-4b5d-abb8-46e966e14b78")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
