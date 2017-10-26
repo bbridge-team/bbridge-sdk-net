29836cace1b2087fe0c5faaa3f7418704e8af7b7
Kirill Lepikhin <lepihin.k@gmail.com>
2017-10-26 13:42:06 +0300

    Revert framework to 1.1

--- bBridgeAPISDK-NETCore.Test/TestResources.Designer.cs
+++ bBridgeAPISDK-NETCore.Test/TestResources.Designer.cs
@@ -10,6 +10,7 @@
 
 namespace bBridgeAPISDKNETCore.Test {
     using System;
+    using System.Reflection;
     
     
     /// <summary>
@@ -39,7 +40,7 @@ namespace bBridgeAPISDKNETCore.Test {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("bBridgeAPISDKNETCore.Test.TestResources", typeof(TestResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("bBridgeAPISDKNETCore.Test.TestResources", typeof(TestResources).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
